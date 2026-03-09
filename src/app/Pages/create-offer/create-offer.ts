67d7873c0f2054f7fc69081e8f91add4b60fffa8
Yehiea Mostafa <yehieamostafa10@gmail.com>
2026-03-09 15:29:51 +0200

    Second

--- src/app/Pages/create-offer/create-offer.ts
+++ src/app/Pages/create-offer/create-offer.ts
@@ -12,43 +12,50 @@ import { ToastrService } from 'ngx-toastr';
   styleUrl: './create-offer.css',
 })
 export class CreateOffer {
-private readonly offerServices = inject(OfferServices);
-    private readonly ToastrService=inject(ToastrService);
 
-offerForm: FormGroup = new FormGroup({
-  buyQuantity: new FormControl(''),
-  getQuantity: new FormControl(''),
-  startDate: new FormControl(''),
-  durationDays: new FormControl(''),
-  productId: new FormControl('')
-});
+  private readonly offerServices = inject(OfferServices);
+  private readonly ToastrService = inject(ToastrService);
 
-CreateOffer(offer: IOffer): void {
-  this.offerServices.CreateOffer(offer).subscribe({
+  today: string = new Date().toISOString().split('T')[0];
 
-    next: (response) => {
-      console.log(response);
-      this.ToastrService.success("Create Offer","Create Offer Successfully");
-    },
-
-    error: (error) => {
-      console.log(error.error);   // هنا ستظهر رسالة السيرفر
-      this.ToastrService.error("Fail Create Offer","Create Offer Fail");
-    }
-
+  offerForm: FormGroup = new FormGroup({
+    buyQuantity: new FormControl(''),
+    getQuantity: new FormControl(''),
+    startDate: new FormControl(this.today), // تاريخ اليوم تلقائي
+    durationDays: new FormControl(''),
+    productId: new FormControl('')
   });
+
+  CreateOffer(offer: IOffer): void {
+
+    this.offerServices.CreateOffer(offer).subscribe({
+
+      next: (response) => {
+        console.log(response);
+        this.ToastrService.success("Create Offer", "Create Offer Successfully");
+      },
+
+      error: (error) => {
+        console.log(error.error);
+        this.ToastrService.error("Fail Create Offer", "Create Offer Fail");
+      }
+
+    });
+
+  }
+
+  submitOffer(): void {
+
+    const offer: IOffer = {
+      buyQuantity: Number(this.offerForm.value.buyQuantity),
+      getQuantity: Number(this.offerForm.value.getQuantity),
+      startDate: this.offerForm.value.startDate,
+      durationDays: Number(this.offerForm.value.durationDays),
+      productId: Number(this.offerForm.value.productId)
+    };
+
+    this.CreateOffer(offer);
+
+  }
+
 }
-
-submitOffer(): void {
-
-const offer: IOffer = {
-  buyQuantity: Number(this.offerForm.value.buyQuantity),
-  getQuantity: Number(this.offerForm.value.getQuantity),
-  startDate: this.offerForm.value.startDate,   // بدون T00:00:00
-  durationDays: Number(this.offerForm.value.durationDays),
-  productId: Number(this.offerForm.value.productId)
-};
-
-this.CreateOffer(offer);
-
-}}
